e7a4d23b4d2320a26c112b09992c28629a875d80
Dmitriy Haletskiy <haleckijd98@gmail.com>
2026-04-24 15:37:07 +0700

    feat(db): add db connetion settings

---
+++ src/database/core.py
@@ -0,0 +1,9 @@
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from src.config import settings
+
+engine = create_async_engine(settings.DATABASE_URL, echo=True)
+
+async_session_maker = async_sessionmaker(
+    bind=engine, expire_on_commit=False, class_=AsyncSession
+)
