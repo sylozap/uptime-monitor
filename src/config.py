ca554de7762311752e0d271086e2dad7b262766f
Dmitriy Haletskiy <haleckijd98@gmail.com>
2026-04-24 15:32:40 +0700

    feat: add db url to config

--- src/config.py
+++ src/config.py
@@ -18,5 +18,7 @@ class Settings(BaseSettings):
         extra="ignore",
     )
 
+    DATABASE_URL: str = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
 
 settings = Settings()
