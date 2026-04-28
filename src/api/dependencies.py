4c10002e9da29ee54d3d68dd527381e0aa045a83
Dmitriy Haletskiy <haleckijd98@gmail.com>
2026-04-28 15:16:28 +0700

    feat: add current_user dependency

---
+++ src/api/dependencies.py
@@ -0,0 +1,43 @@
+from typing import Annotated
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+
+from src.core.security import decode_access_token
+from src.models.user import User
+from src.repositories.dependencies import get_user_repository
+from src.repositories.user_repository import UserRepository
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
+
+
+async def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)],
+    user_repository: Annotated[UserRepository, Depends(get_user_repository)],
+) -> User:
+
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail={"message": "Could not validate credentials"},
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    payload = decode_access_token(token)
+
+    if payload is None:
+        raise credentials_exception
+
+    user_id = payload.get("sub")
+
+    if user_id is None:
+        raise credentials_exception
+
+    user = await user_repository.get_by_id(user_id)
+
+    if user is None:
+        raise credentials_exception
+
+    return user
+
+
+CurrentUserDep = Annotated[User, Depends(get_current_user)]
