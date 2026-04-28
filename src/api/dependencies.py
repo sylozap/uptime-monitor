5ee5daea852c04e93c31341973e20a3ad51316be
Dmitriy Haletskiy <haleckijd98@gmail.com>
2026-04-28 23:45:27 +0700

    feat: add more checks, remove exception from dependency

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,8 +1,10 @@
+import uuid
 from typing import Annotated
 
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 
+from src.core.exceptions import InactiveUserError, InvalidTokenError
 from src.core.security import decode_token
 from src.models.user import User
 from src.repositories.dependencies import get_user_repository
@@ -16,29 +18,26 @@ async def get_current_user(
     user_repository: Annotated[UserRepository, Depends(get_user_repository)],
 ) -> User:
 
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail={"message": "Could not validate credentials"},
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-
     payload = decode_token(token)
 
     if payload is None:
-        raise credentials_exception
+        raise InvalidTokenError()
 
     if payload.get("type") != "access":
-        raise credentials_exception
+        raise InvalidTokenError()
 
     user_id = payload.get("sub")
 
     if user_id is None:
-        raise credentials_exception
+        raise InvalidTokenError()
 
-    user = await user_repository.get_by_id(user_id)
+    user = await user_repository.get_by_id(uuid.UUID(user_id))
 
     if user is None:
-        raise credentials_exception
+        raise InvalidTokenError()
+
+    if not user.is_active:
+        raise InactiveUserError()
 
     return user
 
