e769ed8d226710a54eb364d2cf2fee8cd047082b
Dmitriy Haletskiy <haleckijd98@gmail.com>
2026-04-29 10:48:29 +0700

    refactor(core): make db connection params required

--- src/core/config.py
+++ src/core/config.py
@@ -11,11 +11,11 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
 
-    POSTGRES_USER: str = ""
-    POSTGRES_PASSWORD: str = ""
-    POSTGRES_DB: str = ""
-    POSTGRES_HOST: str = ""
-    POSTGRES_PORT: str = ""
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
 
     model_config = SettingsConfigDict(
         env_file=BASE_DIR / ".env", env_file_encoding="utf-8", extra="ignore"
