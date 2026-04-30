4de5bdbb0b685d2369fcb570b9231924272238fb
Dmitriy Haletskiy <haleckijd98@gmail.com>
2026-04-30 15:39:05 +0700

    rename monitors.py router to monitor.py, add monitor_router to  api_router

--- src/api/router.py
+++ src/api/router.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
 
 from src.api.v1.routers.auth import router as auth_router
+from src.api.v1.routers.monitor import router as monitor_router
 
 api_router = APIRouter(prefix="/api/v1")
+
 api_router.include_router(auth_router)
+api_router.include_router(monitor_router)
