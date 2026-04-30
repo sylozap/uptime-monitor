2dc42c26ac98d24214a7224d141ce2d460174028
Dmitriy Haletskiy <haleckijd98@gmail.com>
2026-04-30 15:26:38 +0700

    refactor dependency injection

--- src/services/dependencies.py
+++ src/services/dependencies.py
@@ -2,25 +2,19 @@ from typing import Annotated
 
 from fastapi import Depends
 
-from src.repositories.dependencies import get_monitor_repository, get_user_repository
-from src.repositories.monitor_repository import MonitorRepository
-from src.repositories.user_repository import UserRepository
+from src.repositories.dependencies import MonitorRepositoryDep, UserRepositoryDep
 from src.services.auth_service import AuthService
 from src.services.monitor_service import MonitorService
 
 
-async def get_auth_service(
-    user_repository: Annotated[UserRepository, Depends(get_user_repository)],
-):
+async def get_auth_service(user_repository: UserRepositoryDep):
     return AuthService(user_repository)
 
 
 AuthServiceDep = Annotated[AuthService, Depends(get_auth_service)]
 
 
-async def get_monitor_service(
-    monitor_repository: Annotated[MonitorRepository, Depends(get_monitor_repository)],
-):
+async def get_monitor_service(monitor_repository: MonitorRepositoryDep):
     return MonitorService(monitor_repository)
 
 
