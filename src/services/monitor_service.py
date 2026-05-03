6ec0fa1064a4646492fda262a8e59a5b67530a60
Dmitriy Haletskiy <haleckijd98@gmail.com>
2026-05-03 18:36:24 +0700

    refactor: use already existing service method

--- src/services/monitor_service.py
+++ src/services/monitor_service.py
@@ -42,12 +42,7 @@ class MonitorService:
     async def update_monitor(
         self, id: uuid.UUID, user_id: uuid.UUID, fields_to_update: MonitorUpdate
     ) -> Monitor:
-        monitor = await self.monitor_repository.get_monitor_by_id(
-            id=id, user_id=user_id
-        )
-        if not monitor:
-            raise MonitorNotFoundError()
-
+        monitor = await self.get_monitor_by_id(id=id, user_id=user_id)
         update_data = fields_to_update.model_dump(exclude_unset=True, mode="json")
 
         if not update_data:
@@ -60,11 +55,6 @@ class MonitorService:
         return updated_monitor
 
     async def delete_monitor(self, id: uuid.UUID, user_id: uuid.UUID) -> None:
-        monitor = await self.monitor_repository.get_monitor_by_id(
-            id=id, user_id=user_id
-        )
-
-        if not monitor:
-            raise MonitorNotFoundError()
+        monitor = await self.get_monitor_by_id(id=id, user_id=user_id)
 
         await self.monitor_repository.delete_monitor(monitor=monitor)
