2074e3d72e62713cec9cb0ae9caa0d6b7e6bc35c
Dmitriy Haletskiy <haleckijd98@gmail.com>
2026-05-14 12:13:51 +0700

    feat: add session param to service dependencies

--- src/services/dependencies.py
+++ src/services/dependencies.py
@@ -2,20 +2,23 @@ from typing import Annotated
 
 from fastapi import Depends
 
+from src.database.dependencies import SessionDep
 from src.repositories.dependencies import MonitorRepositoryDep, UserRepositoryDep
 from src.services.auth_service import AuthService
 from src.services.monitor_service import MonitorService
 
 
-async def get_auth_service(user_repository: UserRepositoryDep):
-    return AuthService(user_repository)
+async def get_auth_service(session: SessionDep, user_repository: UserRepositoryDep):
+    return AuthService(session, user_repository)
 
 
 AuthServiceDep = Annotated[AuthService, Depends(get_auth_service)]
 
 
-async def get_monitor_service(monitor_repository: MonitorRepositoryDep):
-    return MonitorService(monitor_repository)
+async def get_monitor_service(
+    session: SessionDep, monitor_repository: MonitorRepositoryDep
+):
+    return MonitorService(session, monitor_repository)
 
 
 MonitorServiceDep = Annotated[MonitorService, Depends(get_monitor_service)]
